c6f956ec2e5a001e487427b562fc3d93594c4429
Eduardo Sa <eduardo.sa.022@gmail.com>
2025-05-18 16:50:44 -0300

    feat: :sparkles: criação do model Shop

---
+++ src/models/shop.tsx
@@ -0,0 +1,7 @@
+export class Shop {
+    constructor(
+        public title: string,
+        public price: string,
+        public date: string,
+    ){}
+}
--- src/screens/home/homeScreen.tsx
+++ src/screens/home/homeScreen.tsx
@@ -1,22 +1,15 @@
 import Header from '@/src/components/header';
+import { shops } from '@/src/constants/shops';
 import React from 'react';
 import { ScrollView, StyleSheet } from 'react-native';
 import ShopCard from './components/shopCard';
 
 export default function HomeScreen() {
 
-  const mockData = [
-    { title: 'Compra do mês 1', price: 'R$700.00', date: '17/01/25' },
-    { title: 'Compra do mês 2', price: 'R$743.00', date: '17/02/25' },
-    { title: 'Compra do mês 3', price: 'R$624.00', date: '17/03/25' },
-    { title: 'Compra do mês 4', price: 'R$700.00', date: '17/04/25' },
-    { title: 'Compra do mês 5', price: 'R$811.00', date: '17/05/25' },
-  ];
-
   return (
     <ScrollView style={styles.scroll}>
       <Header />
-      {mockData.map((item, index) => (
+      {shops.map((item, index) => (
         <ShopCard key={index} {...item} />
       ))}
     </ScrollView>
