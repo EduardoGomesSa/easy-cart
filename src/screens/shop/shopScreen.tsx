378deccf7805226cdd7816cc768147e01f6564f5
Eduardo Sa <eduardo.sa.022@gmail.com>
2025-05-18 12:34:57 -0300

    feat: :sparkles: parametros sendo exibidos

--- src/screens/shop/shopScreen.tsx
+++ src/screens/shop/shopScreen.tsx
@@ -1,16 +1,26 @@
+import { useLocalSearchParams } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function ShopScreen() {
+    const { title, price, date} = useLocalSearchParams();
     return (
         <View style={styles.container}>
-            <Text>Tela de detalhes de uma compra</Text>
+            <Text style={styles.text}>Compra: {title}</Text>
+            <Text style={styles.text}>Preço: {price}</Text>
+            <Text style={styles.text}>Data: {date}</Text>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: 'yellow',
-        alignContent: 'center',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff'
+    },
+    text: {
+        fontSize: 20,
+        marginVertical: 10,
     }
 })
