691676e2a9ad26aba59800fe78590d21e3c58eaf
Eduardo Sa <eduardo.sa.022@gmail.com>
2025-05-19 22:37:47 -0300

    fix: :bug: resolvendo problema do posicionamento do botão na tela

--- src/screens/home/homeScreen.tsx
+++ src/screens/home/homeScreen.tsx
@@ -2,29 +2,32 @@ import AddButton from '@/src/components/addButton';
 import Header from '@/src/components/header';
 import { shops } from '@/src/constants/shops';
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet, View } from 'react-native';
 import ShopCard from './components/shopCard';
 
 export default function HomeScreen() {
 
   return (
-    <ScrollView style={styles.scroll}>
+    <View style={styles.container}>
+      <ScrollView style={styles.scroll}>
       <Header />
       {shops.map((item, index) => (
         <ShopCard key={index} {...item} />
       ))}
-      <AddButton onPress={()=>{}} />
+      
     </ScrollView>
+    <AddButton onPress={()=>{}} />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white'
+    position: 'relative',
+    backgroundColor: 'white',
   },
   scroll: {
     paddingBottom: 16,
-    backgroundColor: 'white'
   }
 });
