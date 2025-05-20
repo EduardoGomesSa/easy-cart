0bd289b01871701aef6625f2480fb4f625285a8d
Eduardo Sa <eduardo.sa.022@gmail.com>
2025-05-19 22:06:01 -0300

    feat: :sparkles: criação do model item

---
+++ src/models/item.ts
@@ -0,0 +1,9 @@
+export class Item {
+    constructor(
+        public id: number,
+        public name: string,
+        public price: number,
+        public amount: number,
+        public type_unit: number
+    ){}
+}
--- src/models/shop.ts
+++ src/models/shop.ts
@@ -1,7 +1,11 @@
+import { Item } from "./item";
+
 export class Shop {
     constructor(
+        public id: number,
         public title: string,
         public price: string,
         public date: string,
-    ){}
+        public items: Item[]
+    ) { }
 }
